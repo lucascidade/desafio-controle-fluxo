5c801581951a2ee51ebd642c61c9fcb8c86336aa
lucascidade <lucascidade96@gmail.com>
2024-04-29 11:33:10 -0400

    feat: desafio controle de fluxo básico java, santander btcamp 2024

---
+++ src/Contador.java
@@ -0,0 +1,32 @@
+import exception.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("Numero 1 deve ser MAIOR que Numero 2");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 0; i < contagem; i++){
+            System.out.print("imprimindo numero de interação nº" + i + "\n");
+        }
+    }
+}
---
+++ src/exception/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem ){
+        super(mensagem);
+    }
+
+}
